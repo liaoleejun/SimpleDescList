ba164bdc1a3eeb32e866f02d15ffeed3dbaa970e
Leejun Liao <liaoleejun@163.com>
2019-03-04 13:55:19 +0800

    解耦 & 重命名变量
    * 因为本项目是一个大项目的子项目，所以为了更好的维护二者代码，本项目的代码须与大项目的代码之间解耦。即不直接使用大项目的东西，而是通过一个接口来适配大项目：这样如果大项目修改了，那么本项目不需要修改，只须改这个适配接口即可；同理，如果本项目修改了，大项目的代码不必修改，同样也是修改适配接口即可。解耦的本质大概就是通过适配接口来达到上下层级的
    * 重命名变量；名称通逻辑，名称有多么达意，逻辑就有多通
    * 判断如果没有description时，不显示description的标题
    * 添加跳转到锚点时，高亮内容

--- script/SimpleDescList.js
+++ script/SimpleDescList.js
@@ -1,30 +1,40 @@
 $(document).ready(function () {
-    /* 1. 描述排序: 按照引用在 HTML 文档中出现顺序, 把引用的 data-ref 生成无重复值数组
-          descsOrdered */
-    let descs = $("[data-dt]");
+    /* 1. 描述排序
+     *
+     *    按照引用在 HTML 文档中出现顺序, 把引用的 data-ref 生成无重复值数组
+     *    descsOrdered
+     */
+
+    let descs = $(".description");
+
     // descsOrdered 是由 data-ref 构成的无重复数组，按照在HTML文档中出现的顺序
-    let descsOrdered = [];
-    let descsConceptOrdered = [];
+    let descsKeyOrdered = [];
+    let descsValueOrdered = [];
     for (let i = 0; i < descs.length; i++) {
-        let dataRef = descs[i].getAttribute('data-dt');
+        let dataRef = descs[i].getAttribute('href');
         let concept = descs[i].innerHTML;
         // array push only if not exist
-        descsOrdered.indexOf(dataRef) === -1 ? descsOrdered.push(dataRef):
+        descsKeyOrdered.indexOf(dataRef) === -1 ? descsKeyOrdered.push(dataRef):
             console.log(dataRef + " already exists");
-        descsConceptOrdered.indexOf(concept) === -1 ?
-            descsConceptOrdered.push(concept):
+        descsValueOrdered.indexOf(concept) === -1 ?
+            descsValueOrdered.push(concept):
             console.log(concept + " already exists");
     }
     
-    /* 2. 生成排好序的 Description list */
-    $(document.body).append("<div><h2>Description List</h2><ul id='descs'></ul>" +
-        "</div>");
-    for (let i = 0; i < descsOrdered.length; i++) {
-        let id = descsOrdered[i];
-        let ttt = document.createElement('li');
-        ttt.innerHTML = "<b>" + descsConceptOrdered[i] + "</b>";
-        // TODO: jquery.min.js:2 Uncaught TypeError: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node'.
-        ttt.appendChild(document.getElementById(id));
-        document.getElementById("descs").appendChild(ttt);
+    /* 2. 生成排好序的描述，附在最后 */
+    if (descsKeyOrdered.length > 0) {
+        console.log(descsKeyOrdered);
+
+        $(document.body).append("<div><h2>Description List</h2><ul id='descriptions-append'></ul>" +
+            "</div>");
+        for (let i = 0; i < descsKeyOrdered.length; i++) {
+            let id = descsKeyOrdered[i];
+            id = id.substring(1, id.length);
+            let ttt = document.createElement('li');
+            ttt.innerHTML = "<b>" + descsValueOrdered[i] + "</b>";
+            // TODO: jquery.min.js:2 Uncaught TypeError: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node'.
+            ttt.appendChild(document.getElementById(id));
+            document.getElementById("descriptions-append").appendChild(ttt);
+        }
     }
 });
